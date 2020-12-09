7cc9fd860fe367a5a36db67d4ce1f188f1478c21
Koshchiy <a.koshchiy@pixonic.com>
2020-12-09 23:18:18 +0300

    project1 tests passed!

--- proj1/src/bin/kvs.rs
+++ proj1/src/bin/kvs.rs
@@ -1,5 +1,28 @@
+use std::env;
+use clap::{Arg, App, load_yaml};
+use std::fs::read_to_string;
+use std::process::exit;
+
+
 fn main() {
-    // let x = 5;
-    // println!("Hello, world!");
-    panic!("no implemented");
+    let yaml = load_yaml!("cli.yml");
+    let app = App::from(yaml);
+    let matches = app.get_matches();
+
+    if matches.is_present("version") {
+        println!(env!("CARGO_PKG_VERSION"));
+    }
+
+    match matches.subcommand() {
+        Some(("get", args)) => { error_exit("unimplemented"); }
+        Some(("set", args)) => { error_exit("unimplemented"); }
+        Some(("rm", args)) => { error_exit("unimplemented"); }
+        Some((cmd, args)) => { error_exit(format!("{}{}", "unknown command: ", cmd).as_str()); }
+        _ => { unreachable!(); }
+    }
+}
+
+fn error_exit(msg: &str) {
+    eprintln!("{}", msg);
+    exit(1);
 }
