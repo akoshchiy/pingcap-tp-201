4d0002008e4b3f2cabfd3bd0773d1b5698d0fcef
Koshchiy <a.koshchiy@pixonic.com>
2021-08-26 20:47:58 +0300

    proj2 start bin

--- proj2/src/bin/kvs.rs
+++ proj2/src/bin/kvs.rs
@@ -5,63 +5,39 @@ use std::process::exit;
 
 use std::error::Error;
 use std::fs::File;
-use thiserror::Error;
-// use std::backtrace::Backtrace;
-
-#[derive(Error, Debug)]
-enum MyError {
-    #[error(transparent)]
-    Test { #[from] source: Box<dyn Error> },
-}
+use proj2::kvs;
 
 fn main() {
-    // let store = KvStore::open("sdad").unwrap();
+    let dir = env::current_dir().unwrap();
+    let mut store = KvStore::open(dir).unwrap();
 
-    match test_err() {
-        Ok(_) => return,
-        Err(e) => {
-            eprintln!("error: {}", e);
-            exit(1);
+    let yaml = load_yaml!("cli.yml");
+    let app = App::from(yaml);
+    let matches = app.get_matches();
+
+    if matches.is_present("version") {
+        println!(env!("CARGO_PKG_VERSION"));
+    }
+
+    match matches.subcommand() {
+        Some(("get", args)) => {
+            let key = args.value_of("key").unwrap();
+            let result = store.get(key.to_string())
+                .unwrap()
+                .unwrap_or("Key not found".to_string());
+            println!("{}", result);
+        }
+        Some(("set", args)) => {
+            let key = args.value_of("key").unwrap();
+            let value = args.value_of("value").unwrap();
+            store.set(key.to_string(), value.to_string()).unwrap();
+        }
+        Some(("rm", args)) => {
+            let key = args.value_of("key").unwrap();
+            store.remove(key.to_string()).unwrap();
+        }
+        _ => {
+            unreachable!();
         }
     }
-
-    // let yaml = load_yaml!("cli.yml");
-    // let app = App::from(yaml);
-    // let matches = app.get_matches();
-    //
-    // if matches.is_present("version") {
-    //     println!(env!("CARGO_PKG_VERSION"));
-    // }
-    //
-    // match matches.subcommand() {
-    //     Some(("get", _args)) => {
-    //         error_exit("unimplemented");
-    //     }
-    //     Some(("set", _args)) => {
-    //         error_exit("unimplemented");
-    //     }
-    //     Some(("rm", _args)) => {
-    //         error_exit("unimplemented");
-    //     }
-    //     _ => {
-    //         unreachable!();
-    //     }
-    // }
-}
-
-fn test_err() -> Result<(), MyError> {
-    match File::open("foo.txt") {
-        Ok(_) => Ok(()),
-        Err(e) => Err(MyError::Test {
-            source: Box::new(e)
-            // msg: "fucked".to_string(),
-            // source: Box::new(e),
-            // backtrace: Backtrace::capture(),
-        }),
-    }
-}
-
-fn error_exit(msg: &str) {
-    eprintln!("{}", msg);
-    exit(1);
 }
