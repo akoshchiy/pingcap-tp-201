d31be3b82500ffeb0ba0f37745fbda0aefd6b409
Koshchiy <a.koshchiy@pixonic.com>
2021-09-06 02:21:58 +0300

    proj3 - kvs-server init

--- proj3/src/bin/kvs-server.rs
+++ proj3/src/bin/kvs-server.rs
@@ -1,6 +1,10 @@
 use clap::{load_yaml, App};
 use std::net::IpAddr;
 use std::str::FromStr;
+use proj3::kvs::{KvsServer, KvStore, SledKvsEngine, KvsEngine, Result, KvError};
+use std::path::Path;
+use sled::Db;
+use std::env;
 
 fn main() {
     let yaml = load_yaml!("kvs-client.yml");
@@ -12,11 +16,36 @@ fn main() {
         return;
     }
 
+    let dir = env::current_dir().unwrap();
+
     let addr = parse_addr(matches.value_of("addr").unwrap_or("localhost:4000"));
 
-    let engine = matches.value_of("engine").unwrap_or("kvs");
+    let engine_name = matches.value_of("engine").unwrap_or("kvs");
+
+    start_server(engine_name, dir.as_path(), addr)
+        .expect("server start failed");
 }
 
 fn parse_addr(addr: &str) -> IpAddr {
     IpAddr::from_str(addr).expect("bad addr format")
+}
+
+fn start_server(engine: &str, root_path: &Path, addr: IpAddr) -> Result<()> {
+    match engine {
+        "kvs" => build_kvs(root_path)
+            .and_then(|e| KvsServer::new(e).listen(addr)),
+        "sled" => build_sled(root_path).and_then(|e| KvsServer::new(e).listen(addr)),
+        _ => panic!("undefined engine: {}", engine)
+    }
+}
+
+fn build_sled(file_path: &Path) -> Result<impl KvsEngine> {
+    sled::open(file_path)
+        // TODO new err type for sled init
+        .map_err(|err| KvError::Sled { key: String::new(), source: err })
+        .map(|db| SledKvsEngine::new(db))
+}
+
+fn build_kvs(file_path: &Path) -> Result<impl KvsEngine> {
+    KvStore::open(file_path)
 }
--- proj3/src/kvs/server/mod.rs
+++ proj3/src/kvs/server/mod.rs
@@ -1,3 +1,24 @@
+use crate::kvs::KvsEngine;
+use std::net::IpAddr;
+use crate::kvs::Result;
+
 pub mod engine;
 
-pub struct KvsServer {}
+pub struct KvsServer<E: KvsEngine> {
+    engine: E,
+    // addr: IpAddr,
+}
+
+impl<E: KvsEngine> KvsServer<E> {
+    pub fn new(engine: E) -> KvsServer<E> {
+        KvsServer {
+            engine
+        }
+    }
+
+    pub fn listen(&self, addr: IpAddr) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+
